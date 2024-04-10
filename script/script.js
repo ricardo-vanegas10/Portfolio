271f69688066bab453f013b43f76752ef198ec71
Ricardo Vanegas <145631617+ricardo-vanegas10@users.noreply.github.com>
2024-04-09 19:54:39 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function () {
           <li>Desarrollo de piezas digitales, mailings, y landings.</li>
           <li>Maquetación y desarrollo de piezas mailings.</li>
       </ul>
-      <br> */
+      <br> 
                 <h5>Auxiliar de ingeniero civil</h5>
                 <p>Meg Obras SAS</p>
                 <p>Enero 2022 - Julio 2022</p>
@@ -159,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         identificar y resolver problemas que surjan en el proyecto.</li>
                 </ul>
                 <br>
-              /*  <h5>Auxiliar de ingeniero civil</h5>
+              <h5>Auxiliar de ingeniero civil</h5>
                 <p>B3D Asociados Consultores y Construcciones S.A.S.</p>
                 <p>Enero 2021 - Diciembre 2021</p>
                 <ul>
@@ -169,7 +169,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <li>Estudiar y conocer a cabalidad los planos y especificaciones de la obra y solicitar autorización
                         para realizar modificaciones al proyecto cuando este lo requiera.</li>
                 </ul>
-                <br> */
+                <br> 
                 <h5>Auxiliar de ingeniero civil</h5>
                 <p>Construcciones Piedrahita</p>
                 <p>Junio 2017 - Agosto 2017</p>
@@ -366,11 +366,11 @@ document.addEventListener('DOMContentLoaded', function () {
       <p>Syntactic Academy</p>
       <p>December 2023</p>
       <ul>
-        /*  <li>Front-end development</li>
+          <li>Front-end development</li>
           <li>Development of digital pieces, mailings, and landings.</li>
           <li>Layout and development of mailing pieces.</li>
       </ul>
-      <br> */
+      <br> 
                 <h5>Civil engineer assistant</h5>
                 <p>Meg Obras SAS</p>
                 <p>January 2022 - July 2022</p>
@@ -380,7 +380,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <li>Apply technical knowledge of civil engineering principles and practices to identify and resolve problems that arise in the project.</li>
                 </ul>
                 <br>
-              /*  <h5>Civil engineer assistant</h5>
+                <h5>Civil engineer assistant</h5>
                 <p>B3D Asociados Consultores y Construcciones S.A.S.</p>
                 <p>January 2021 - December 2021</p>
                 <ul>
@@ -388,7 +388,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <li>Review the established technical specifications and the corresponding legal regulations, carrying out the respective quality controls.</li>
                     <li>Study and fully understand the plans and specifications of the work and request authorization to make modifications to the project when required.</li>
                 </ul>
-                <br> */
+                <br> 
                 <h5>Civil engineer assistant</h5>
                 <p>Piedrahita Constructions</p>
                 <p>June 2017 - August 2017</p>
